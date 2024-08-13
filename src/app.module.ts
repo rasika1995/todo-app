a33f7d043f8d1f4408a10b5c8486bb67a37aa794
Rasika Priyadarshani <rasikap@alliontechnologies.com>
2024-08-13 17:44:24 +0530

    integrate ConfigModule for environment variable management

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/todo-db'), // Replace with your MongoDB connection string
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigModule available globally, no need to import in other modules
+      envFilePath: '.env', // Specifies the path to your .env file (optional, default is '.env')
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
     TodoModule,
   ],
   controllers: [AppController],
